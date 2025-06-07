83bde197ad53ed120ef192339e0b53c5120c0373
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2025-06-07 21:13:23 +0630

    init

---
+++ chapter1/dynamic.js
@@ -0,0 +1,6 @@
+let a = 10;
+console.log('A ',a);
+a = "Hello";
+console.log('A ',a);
+a = 3 / 2
+console.log('A ',a);
---
+++ chapter1/static.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	fmt.Println("A is ", a)
+
+	//a = "Hello"
+	fmt.Println("A is ", a)
+
+	a = 3 / 2
+	fmt.Println("A is ", a)
+
+}
